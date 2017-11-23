c3e70a2d74126d473e861486a191276eb1af12fe
Volodymyr Fuchenko <volodymyr.fuchenko@gmail.com>
2017-11-23 08:25:58 +0200

    Separate examples for SoftwareSerial and HardwareSerial

---
+++ examples/hw_get_values/hw_get_values.ino
@@ -0,0 +1,23 @@
+#include <SoftwareSerial.h>
+#include <MHZ19.h>
+
+MHZ19 mhz(&Serial1);
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println(F("Starting..."));
+
+  Serial1.begin(9600);
+}
+
+void loop()
+{
+  Serial.print(F("CO2: "));
+  Serial.println(mhz.getCO2());
+  Serial.print(F("Temperature: "));
+  Serial.println(mhz.getTemperature());
+  Serial.print(F("Accuracy: "));
+  Serial.println(mhz.getAccuracy());
+  delay(15000);
+}
